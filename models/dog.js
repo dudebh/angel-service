5f03cfe8dc70c953ae4f90ef1d14afe109f360a6
jho9595 <rathalosking95@gmail.com>
2021-04-23 02:14:58 +0700

    jourdy

--- models/dog.js
+++ models/dog.js
@@ -4,19 +4,24 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Dog extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
+      Dog.belongsToMany( models.User, { through: models.Transaction})
     }
   };
   Dog.init({
     name: DataTypes.STRING,
     species: DataTypes.STRING,
-    birthYear: DataTypes.INTEGER,
+    birthYear: {
+      type: DataTypes.INTEGER,
+      validate: {
+        mustBeMoreThanOneYearOld () {
+          if ((this.birthYear + 1 ) == new Date().getFullYear()) {
+            throw new Error(`Not mature enough for the profesional cannine world`)
+          }
+        }
+      }
+    },
     gender: DataTypes.STRING,
     speciality: DataTypes.STRING,
     availability: DataTypes.BOOLEAN,
--- models/user.js
+++ models/user.js
@@ -4,14 +4,9 @@ const {
 } = require('sequelize');
 const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  class User extends Model {    
     static associate(models) {
-      // define association here
+      User.belongsToMany( models.Dog, { through: models.Transaction})
     }
   };
   User.init({
